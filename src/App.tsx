fe5fd6e046373bf593d3ada3b83398ff7c15b6b2
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-06-12 14:07:28 +0300

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppTitle/>
+            <Raiting/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Raiting() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>
+            <div>Star</div>
+        </div>
+    )
+}
+
+function AppTitle() {
+    return (
+        <>This is APP component</>
+    )
+}
+
+function Accordion() {
+    return (<div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return (
+        <div><h3>Меню</h3></div>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
