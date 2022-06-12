49e7b597a2c54e64199bd113e87ff1464e6e75a1
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-06-12 17:44:28 +0300

    restructurization

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,24 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import Raiting from "./components/Raiting/Raiting";
+import PageTitle from "./components/PageTitle/PageTitle";
+
 
 function App() {
     return (
-        <div className="App">
-            <AppTitle/>
-            <Raiting/>
-            <Accordion/>
+        <div>
+            <PageTitle title={'This is APP component'}/>
+            Article 1
+            <Raiting value={1}/>
+            <Accordion title={'Menu'}/>
+            Article 2
+            <Raiting value={1}/>
+            <Raiting value={2}/>
+            <Raiting value={3}/>
+            <Raiting value={4}/>
+            <Raiting value={5}/>
         </div>
     );
 }
-
-function Raiting() {
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    )
-}
-
-function Star() {
-    return (
-        <div>
-            <div>Star</div>
-        </div>
-    )
-}
-
-function AppTitle() {
-    return (
-        <>This is APP component</>
-    )
-}
-
-function Accordion() {
-    return (<div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
-
-function AccordionTitle() {
-    return (
-        <div><h3>Меню</h3></div>
-    )
-}
-
-function AccordionBody() {
-    return (
-        <div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    )
-}
-
 export default App;
