a5f14984292d54badd7db8c2413c543f126f5311
Alex-Fes <aleksei.fesenko86@gmail.com>
2022-10-24 18:59:04 +0300

    useEffect, cleanup

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -10,26 +10,32 @@ export const Example1 = () => {
     const [counter, setCounter] = useState<number>(1);
     console.log('Example1 was called')
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect every render')
         document.title = counter.toString()
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect only first render (componentDidMount)')
         document.title = counter.toString()
-    } , [])
+    }, [])
 
-    useEffect(()=>{
-        console.log('useEffect first render and every counter change' )
+    useEffect(() => {
+        console.log('useEffect first render and every counter change')
         document.title = counter.toString()
-    } , [counter])
+    }, [counter])
 
 
     return <>
-      Hello, {counter} - {fake}
-        <button onClick={() => {setFake(fake + 1)}}>fake +</button>
-        <button onClick={() => {setCounter(counter + 1)}}>counter +</button>
+        Hello, {counter} - {fake}
+        <button onClick={() => {
+            setFake(fake + 1)
+        }}>fake +
+        </button>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>counter +
+        </button>
     </>
 };
 
@@ -45,17 +51,71 @@ export const SetTimeoutExample = () => {
     //     }, 1000)
     // }, [counter])
 
-    useEffect(()=> {
-        setInterval(()=>{
+    useEffect(() => {
+        setInterval(() => {
             setCounter(state => state + 1)
         }, 1000)
     }, [])
 
     return <>
         Hello, counter: {counter} - fake: {fake}
-        {/*<button onClick={() => {setFake(fake + 1)}}>fake +</button>*/}
-        {/*<button onClick={() => {setCounter(counter + 1)}}>counter +</button>*/}
+
     </>
 };
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState<number>(1);
+    console.log('Component rendered')
+    useEffect(() => {
+        console.log('Effect occurred')
+        return () => {
+            console.log('RESET EFFECT')
+        }
+    }, [])
+    const increase = () => {
+        setCounter(counter + 1)
+    }
+    return <>
+        Hello, counter: {counter}
+        <button onClick={increase}>+</button>
+
+    </>
+};
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState('');
+    console.log('Component rendered with ' + text)
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+        window.document.addEventListener('keypress',handler )
+        return () => {
+            window.document.removeEventListener('keypress', handler)
+        }
+    }, [text])
+
+    return <>
+        Typed text: {text}
+    </>
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
